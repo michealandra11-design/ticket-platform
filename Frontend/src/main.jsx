7c0c20a6c874fb35aba94a515d91fc8c523b52ee
michealandra11-design <michealandra11@gmail.com>
2026-09-19 08:11:01 +0100

    Add main.jsx entrypoint

---
+++ Frontend/src/main.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      primary: "#026cdf",
+      secondary: "#ffffff",
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
+  </React.StrictMode>
+);
